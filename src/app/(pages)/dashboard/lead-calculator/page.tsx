9d528c6d5d399259eb623d71caf89c40ea3965c9
Yasir Mahmood <yasir.mahmood.3795@gmail.com>
2025-10-19 21:46:58 +0500

    added the lead calculator page

---
+++ src/app/(pages)/dashboard/lead-calculator/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LeadCalculatorPage from '@/views/lead-calculator';
+
+const LeadCalculator = () => {
+  return (
+    <LeadCalculatorPage />
+  )
+}
+
+export default LeadCalculator;
---
+++ src/views/lead-calculator/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LeadCalculatorPage = () => {
+  return (
+    <div>LeadCalculator 2</div>
+  )
+}
+
+export default LeadCalculatorPage;
