9b508caac9421e9d765d130bbfcc9110748b8d71
Yasir Mahmood <yasir.mahmood.3795@gmail.com>
2025-10-21 13:28:24 +0500

    added the middleware.ts"

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,49 @@
+// const nextConfig = {
+//   webpack(config: any) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ["@svgr/webpack"],
+//     });
+
+//     return config;
+//   },
+//   experimental: {
+//     turbo: {
+//       rules: {
+//         "*.svg": {
+//           loaders: ["@svgr/webpack"],
+//           as: "*.js",
+//         },
+//       },
+//     },
+//   },
+//   matcher: [
+//     "/((?!api|_next/static|_next/image|favicon.ico).*)", // protect all pages except static/api files
+//   ],
+// };
+
+// module.exports = nextConfig;
+
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config: any) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
-
     return config;
   },
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
+
+  // Use Turbopack configuration properly (optional)
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
       },
     },
   },
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico).*)", // protect all pages except static/api files
-  ],
 };
 
 module.exports = nextConfig;
