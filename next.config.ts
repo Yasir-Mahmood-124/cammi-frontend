fade7885c52abb01efe482b905ee266c343e5b08
Yasir Mahmood <yasir.mahmood.3795@gmail.com>
2025-10-21 14:45:28 +0500

    update the next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,43 @@
-// const nextConfig = {
-//   webpack(config: any) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ["@svgr/webpack"],
-//     });
-
-//     return config;
-//   },
-//   experimental: {
-//     turbo: {
-//       rules: {
-//         "*.svg": {
-//           loaders: ["@svgr/webpack"],
-//           as: "*.js",
-//         },
-//       },
-//     },
-//   },
-//   matcher: [
-//     "/((?!api|_next/static|_next/image|favicon.ico).*)", // protect all pages except static/api files
-//   ],
-// };
-
-// module.exports = nextConfig;
-
-
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ disables ESLint in production builds
-  },
   webpack(config: any) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
+
     return config;
   },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*)", // protect all pages except static/api files
+  ],
 };
 
 module.exports = nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true, // ✅ disables ESLint in production builds
+//   },
+//   webpack(config: any) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ["@svgr/webpack"],
+//     });
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
